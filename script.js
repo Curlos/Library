aef6b768c5c3bbf35de2a5b367ca87f9655c507f
Carlos Martinez <curlosmart@gmail.com>
2021-08-05 14:47:35 -0400

    create divs to display results from API

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const libraryDiv = document.getElementById('library')
 const addBookButton = document.getElementById('addBookSubmit')
 const searchInput = document.getElementById('searchInput')
+const searchResults = document.getElementById('searchResults');
 const API_KEY = 'AIzaSyAA44LXlUJizXoq017jBx9Q2eFdI1W6Kng'
 let myLibrary = []
 let searchText = ''
@@ -21,7 +22,49 @@ const fetchFromAPI = async (event) => {
     
     const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchText}&key=${API_KEY}`)
     const data = await response.json()
+    const books = data.items
     console.log(data)
+    displaySearchResults(books.slice(0, 10))
+}
+
+const displaySearchResults = (books) => {
+    searchResults.innerHTML = ''
+    for (let book of books) {
+        console.log(book.volumeInfo)
+        createBookElement(book.volumeInfo)
+    }
+}
+
+const createBookElement = (book) => {
+    if (!book.imageLinks) {
+        return
+    }
+
+    const bookElem = document.createElement('div')
+    const linkElem = document.createElement('a')
+    const imgElem = document.createElement('img')
+    const titleElem = document.createElement('div')
+    const authorElem = document.createElement('div')
+    const ratingsElem = document.createElement('div')
+    const pageCountElem = document.createElement('div')
+
+    linkElem.setAttribute('href', book.canonicalVolumeLink)
+    linkElem.setAttribute('target', '_blank')
+    imgElem.src = book.imageLinks.thumbnail
+    imgElem.classList.add('bookCover')
+    titleElem.textContent = 'Title: ' + book.title
+    authorElem.textContent = 'Author: ' + book.authors[0]
+    ratingsElem.textContent = 'Rating: ' + (book.averageRating != undefined ? book.averageRating : 'No ratings')
+    pageCountElem.textContent = 'Page count: ' + book.pageCount
+    
+    linkElem.append(imgElem)
+    bookElem.append(linkElem)
+    bookElem.append(titleElem)
+    bookElem.append(authorElem)
+    bookElem.append(ratingsElem)
+    bookElem.append(pageCountElem)
+    bookElem.classList.add('bookSearchResult')
+    searchResults.append(bookElem)
 }
 
 const modal = document.querySelector(".modal");
